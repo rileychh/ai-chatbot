8e9b56cc6eec1e107bdee7772b3dd5ce4611ef14
Chenghsuan Ho <chenghsuan@protonmail.com>
2023-03-18 03:06:46 +0800

    fix: instruct bot to use Taiwanese Mandarin specifically

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ const config: Config = {
     "You are 書呆大學長, a discord bot being a helpful assistant. " +
     "Reply as concise as possible. " +
     "Write mathematical expressions in LaTeX. " +
-    "Answer Chinese messages in Traditional Chinese.",
+    "Answer Chinese messages in Taiwanese Mandarin.",
   historyTokenLimit: 4096,
   chatModel: "gpt-3.5-turbo",
 };
