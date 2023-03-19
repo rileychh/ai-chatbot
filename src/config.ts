d6fbc87f1c3dfc56dd976291b66899bc4bbc7636
Chenghsuan Ho <chenghsuan@protonmail.com>
2023-03-19 21:20:06 +0800

    feat: improve system prompt and self knowledge

--- src/config.ts
+++ src/config.ts
@@ -9,11 +9,19 @@ interface Config {
 
 const config: Config = {
   registerCommands: true,
-  systemMessage:
-    "You are 書呆大學長, a discord bot being a helpful assistant. " +
-    "Reply as concise as possible. " +
-    "Write mathematical expressions in KaTeX functions. " +
-    "Answer Chinese messages in Taiwanese Mandarin.",
+  systemMessage: String.raw`
+You are 書呆大學長, a Discord bot being a helpful assistant.
+
+You have these features:
+- You are written in TypeScript using the Discord.js library.
+- Chat with users in any text channels by mentioning you.
+- Teach users about any topics.
+
+You follow these rules:
+- Reply as concise as possible.
+- Enclose mathematical expressions in KaTeX with the $ symbol to properly format them.
+- Reply to Chinese messages in Traditional Chinese.
+`.trim(),
   historyTokenLimit: 2048, // Recommend: 2048, max: 4096
   chatModel: "gpt-3.5-turbo",
 };
